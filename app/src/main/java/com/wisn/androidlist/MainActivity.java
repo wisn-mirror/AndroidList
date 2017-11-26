97aad8997bdbba08bef5d30f10a529fb7ad8de0d
Wisn <wuyishun_kmk@outlook.com>
2017-11-26 17:17:24 +0800

    add SwipeRefreshLayout

--- app/src/main/java/com/wisn/androidlist/MainActivity.java
+++ app/src/main/java/com/wisn/androidlist/MainActivity.java
@@ -24,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
     private List<String> data = new ArrayList<>();
     private RecyclerView mRecyclerView;
     private SwipeRefreshLayout mGrid_swipe_refresh;
+    private LinearLayoutManager mLinearLayoutManager;
+    private RecyclerView.Adapter<RecyclerView.ViewHolder>
+            mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initData() {
-        for (int a = 0; a < 1000; a++) {
+        for (int a = 0; a < 100; a++) {
             data.add("data" + a);
         }
     }
@@ -57,9 +60,9 @@ public class MainActivity extends AppCompatActivity {
                 },3000);
             }
         });
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        mLinearLayoutManager = new LinearLayoutManager(this);
 //        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
-        mRecyclerView.setLayoutManager(linearLayoutManager);
+        mRecyclerView.setLayoutManager(mLinearLayoutManager);
        /*  GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 3);
         gridLayoutManager.setOrientation(GridLayoutManager.HORIZONTAL);
         mRecyclerView.setLayoutManager(gridLayoutManager);*/
@@ -69,6 +72,9 @@ public class MainActivity extends AppCompatActivity {
 ////        staggeredGridLayoutManager.setOrientation(StaggeredGridLayoutManager.HORIZONTAL);
 //        mRecyclerView.setLayoutManager(staggeredGridLayoutManager);
         mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+
+            private int mLastVisibleItem;
+
             @Override
             public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
                 super.onScrollStateChanged(recyclerView, newState);
@@ -78,6 +84,12 @@ public class MainActivity extends AppCompatActivity {
 //                    && lastVisibleItem +2>=mLayoutManager.getItemCount()) {
 //                    new GetData().execute("http://gank.io/api/data/福利/10/"+(++page));
 //                }
+                if(newState==RecyclerView.SCROLL_STATE_IDLE
+                        &&mLastVisibleItem+5>mLinearLayoutManager.getItemCount()){
+
+                    initData();
+                    mAdapter.notifyDataSetChanged();
+                }
 
             }
 
@@ -85,32 +97,32 @@ public class MainActivity extends AppCompatActivity {
             public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
                 super.onScrolled(recyclerView, dx, dy);
                 //获取加载的最后一个可见视图在适配器的位置。
-//                lastVisibleItem = mLayoutManager.findLastVisibleItemPosition();
+                mLastVisibleItem = mLinearLayoutManager.findLastVisibleItemPosition();
             }
         });
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());
-        mRecyclerView.setAdapter(new RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        mAdapter = new RecyclerView.Adapter<RecyclerView.ViewHolder>() {
             @Override
             public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-                if(viewType==0){
+                if (viewType == 0) {
                     MyViewHolder1
                             myviewHodle =
                             new MyViewHolder1(View.inflate(MainActivity.this, R.layout.item_textview, null));
                     myviewHodle.textview.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View view) {
-                            Toast.makeText(MainActivity.this,"textView",Toast.LENGTH_SHORT).show();
+                            Toast.makeText(MainActivity.this, "textView", Toast.LENGTH_SHORT).show();
                         }
                     });
                     return myviewHodle;
-                }else{
+                } else {
                     MyViewHolder2
                             myviewHodle =
                             new MyViewHolder2(View.inflate(MainActivity.this, R.layout.item_imageview, null));
                     myviewHodle.imageview.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View view) {
-                            Toast.makeText(MainActivity.this,"imageview",Toast.LENGTH_SHORT).show();
+                            Toast.makeText(MainActivity.this, "imageview", Toast.LENGTH_SHORT).show();
                         }
                     });
                     return myviewHodle;
@@ -120,11 +132,11 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-                if(holder instanceof MyViewHolder1){
-                    MyViewHolder1  holder1= (MyViewHolder1) holder;
+                if (holder instanceof MyViewHolder1) {
+                    MyViewHolder1 holder1 = (MyViewHolder1) holder;
                     holder1.textview.setText(data.get(position));
-                }else if(holder instanceof  MyViewHolder2){
-                    MyViewHolder2  holder2= (MyViewHolder2) holder;
+                } else if (holder instanceof MyViewHolder2) {
+                    MyViewHolder2 holder2 = (MyViewHolder2) holder;
                     holder2.imageview.setBackgroundResource(R.mipmap.ic_launcher_round);
                 }
             }
@@ -144,7 +156,8 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
 
-        });
+        };
+        mRecyclerView.setAdapter(mAdapter);
     }
 
     class MyViewHolder1 extends RecyclerView.ViewHolder {
